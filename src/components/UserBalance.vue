6f1120d93be1eb89948ddc7f70092f125056e162
Juan David <othanatosa@gmail.com>
2020-12-10 19:26:49 -0500

    tercer cambio

--- src/components/UserBalance.vue
+++ src/components/UserBalance.vue
@@ -20,7 +20,7 @@ export default {
     this.username = this.$route.params.username;
     let self = this;
     axios
-      .get("https://cajero-api.herokuapp.com/user/balance/" + this.username)
+      .get("https://cajero-api1245.herokuapp.com/user/balance/" + this.username)
       .then((result) => {
         self.balance = result.data.balance;
       })
